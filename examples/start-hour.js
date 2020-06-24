e88c81ee70b0e5a307a9fb36264dee1d98c35db1
Drew <glavich_drew@yahoo.com>
2020-06-24 10:54:47 +0900

    Post formating

--- examples/start-hour.js
+++ examples/start-hour.js
@@ -6,5 +6,6 @@ import moment from 'moment';
 import TimePicker from 'rc-time-picker';
 
 ReactDom.render(
-  <TimePicker defaultValue={moment()} showSecond={false} startHour={20} />
-, document.getElementById('__react-content'));
+  <TimePicker defaultValue={moment()} showSecond={false} startHour={20} />,
+  document.getElementById('__react-content'),
+);
